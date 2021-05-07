08f16ed983302f7fbe9a667d5c6a9f854264421f
Felipe <felipepg@uniplaclages.edu.br>
2021-05-06 21:09:30 -0300

    Ajustes no visual

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,38 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ByteBankApp());
 }
 
-class MyApp extends StatelessWidget {
+class ByteBankApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          body: Column(
-            children: <Widget>[
-              Image.asset('images/bytebank_logo.png'),
-              Container(
+        theme: ThemeData(
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          )
+        ),
+        home: Dashboard(),
+            
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png'),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.all(8.0),
                 height: 100,
-                width: 100,
+                width: 150,
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Icon(Icons.people),
-                    Text('Contatos'),
+                    Icon(Icons.people,color: Colors.white,size: 24.0,),
+                    Text('Contatos',style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0) ,),
                   ],
                 ),
-                color: Colors.green,
+                color: Theme.of(context).primaryColor,
 
-              )
-            ]
-          ),
-          appBar: AppBar(
-            title: Text('Dashboard'),
-            backgroundColor: Colors.green[900],
-          ),
-        )
-            
+              ),
+            )
+          ]
+      ),
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
     );
   }
 }
