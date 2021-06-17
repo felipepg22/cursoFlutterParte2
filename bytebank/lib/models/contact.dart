3177e39a8db61d077c1a22a7f58a908fa3d5eb4b
Felipe <felipepg@uniplaclages.edu.br>
2021-06-16 22:05:38 -0300

    Criando banco de dados e metodos para insercao e listagem

--- bytebank/lib/models/contact.dart
+++ bytebank/lib/models/contact.dart
@@ -1,11 +1,27 @@
 class Contact{
+  final int _id;
   final String _name;
   final int _accountNumber;
 
-  Contact(this._name, this._accountNumber);
+  Contact(this._id, this._name, this._accountNumber);
 
   @override
   String toString() {
-    return 'Contact{_name: $_name, _accountNumber: $_accountNumber}';
+    return 'Contact{_id: $_id, _name: $_name, _accountNumber: $_accountNumber}';
+  }
+
+  int getId()
+  {
+    return this._id;
+  }
+
+  String getName()
+  {
+    return this._name;
+  }
+
+  int getAccountNumber()
+  {
+    return this._accountNumber;
   }
 }
